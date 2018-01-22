6933af3ceabf3c70f22c0a569d0b240fad52309d
dsheludko <mityaev.dm@gmail.com>
2018-01-22 11:04:00 +0200

    symfony: v.4.0.3 installed with some bundles

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Makasim\Yadm\Bundle\YadmBundle::class => ['all' => true],
+    Payum\Bundle\PayumBundle\PayumBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+];
