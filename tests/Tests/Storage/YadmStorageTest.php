ebac83681e1fbbda55bd7f431d88a820f2a068f8
dsheludko <mityaev.dm@gmail.com>
2018-02-01 17:05:39 +0200

    migration: tests: fixed risky test - yadm storage

--- tests/Tests/Storage/YadmStorageTest.php
+++ tests/Tests/Storage/YadmStorageTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace App\Tests\Storage;
 
 use Makasim\Yadm\Storage;
-use App\Storage\GatewayConfigStorage;
 use App\Storage\YadmStorage;
+use Payum\Core\Storage\StorageInterface;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 
@@ -22,16 +22,19 @@ class YadmStorageTest extends TestCase
     {
         $rc = new ReflectionClass(YadmStorage::class);
 
-        $this->assertTrue($rc->implementsInterface(GatewayConfigStorage::class));
+        $this->assertTrue($rc->implementsInterface(StorageInterface::class));
     }
 
     /**
-     * @return void
+     * @throws \ReflectionException
      */
     public function testCouldBeConstructedWithYadmStorageAsFirstArgument() : void
     {
         $storageMock = $this->createMock(Storage::class);
 
-        new YadmStorage($storageMock, YadmStorage::DEFAULT_ID_PROPERTY, Storage::class);
+        $storage = new YadmStorage($storageMock, YadmStorage::DEFAULT_ID_PROPERTY, Storage::class);
+
+        $rc = new ReflectionClass($storage);
+        $this->assertTrue($rc->isInstance($storage));
     }
 }
